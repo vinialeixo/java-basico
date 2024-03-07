e36b025d2e3909e303209eccc841cfebc75a581a
vinialeixo <vi_aleixodias@hotmail.com>
2024-03-07 10:58:56 -0300

    methods

---
+++ variaveis-tipo/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        int a,b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b?"verdadeiro":"false";
+
+        System.out.println(resultado);
+    }
+}
