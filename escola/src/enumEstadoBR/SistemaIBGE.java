e3b89d03bdb74b296ee2f04c0921013f641d13e5
vinialeixo <vi_aleixodias@hotmail.com>
2024-03-19 11:07:20 -0300

    enum

---
+++ escola/src/enumEstadoBR/SistemaIBGE.java
@@ -0,0 +1,9 @@
+package enumEstadoBR;
+
+public class SistemaIBGE {
+    public static void main(String[] args) {
+        for(EstadoBrasileiro e: EstadoBrasileiro.values()){
+            System.out.println(e.getSigla() + " - " + e.getNome());
+        }
+    }
+}
