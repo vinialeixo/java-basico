bfdf1384d82518dc630461359733b90af20dab7e
vinialeixo <vi_aleixodias@hotmail.com>
2024-03-23 16:13:16 -0300

    herança: refatoração

--- messenger/src/heranca/Facebook.java
+++ messenger/src/heranca/Facebook.java
@@ -1,26 +1,5 @@
 package heranca;
 
-public class Facebook {
-    public void enviarMensagem() {
-		//primeiro confirmar se esta conectado a internet
-		validarConectadoInternet();
-		
-		System.out.println("Enviando mensagem Facebook");
-		
-		//depois de enviada, salva o histórico da mensagem
-		salvarHistoricoMensagem();
-		
-		
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem Facebook");
-	}
-	
-	//métodos privadas, visíveis somente na classe
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
+public class Facebook extends ServicoMensagemInstantanea {
+
 }
--- messenger/src/heranca/MSNMessenger.java
+++ messenger/src/heranca/MSNMessenger.java
@@ -1,25 +1,4 @@
 package heranca;
-public class MSNMessenger {
-	public void enviarMensagem() {
-		//primeiro confirmar se esta conectado a internet
-		validarConectadoInternet();
-		
-		System.out.println("Enviando mensagem MSN");
-		
-		//depois de enviada, salva o histórico da mensagem
-		salvarHistoricoMensagem();
-		
-		
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem MSN");
-	}
-	
-	//métodos privadas, visíveis somente na classe
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
+public class MSNMessenger extends ServicoMensagemInstantanea{
+
 }
