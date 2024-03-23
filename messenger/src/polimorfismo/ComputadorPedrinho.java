d8909802937017d21ae5479097efb1ebfaa5de87
vinialeixo <vi_aleixodias@hotmail.com>
2024-03-23 16:42:34 -0300

    polimorfismo

---
+++ messenger/src/polimorfismo/ComputadorPedrinho.java
@@ -0,0 +1,27 @@
+package polimorfismo;
+/*
+ * Simulação do uso da classe MSNMessenger
+ */
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		
+		ServicoMensagemInstantanea smi = null;
+		
+		/*
+		    NÃO SE SABE QUAL APP 
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+		String appEscolhido="fbm"; 
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new Facebook();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
+	}
+}
---
+++ messenger/src/polimorfismo/ServicoMensagemInstantanea.java
@@ -0,0 +1,14 @@
+package polimorfismo;
+
+//a classe MSNMessenger é ou representa
+public abstract class ServicoMensagemInstantanea {
+	
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();
+	
+	
+	//somente os filhos conhecem este método
+	protected void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}	
+}
